0b34730e5b71bc4dc1d51ad82bf0b36a14430237
corsen-olbin <corbinolsen97@gmail.com>
2022-07-09 01:23:46 -0500

    more fields to book to show in library

--- lib/reading_list_ex_web/views/profile_book_view.ex
+++ lib/reading_list_ex_web/views/profile_book_view.ex
@@ -1,3 +1,10 @@
 defmodule ReadingListExWeb.ProfileBooksView do
   use ReadingListExWeb, :view
+
+  def separate_author_names(nil), do: ""
+  def separate_author_names(authors_string) do
+    authors_string
+    |> String.split(";")
+    |> Enum.join(", ")
+  end
 end
--- lib/reading_list_ex_web/views/search_view.ex
+++ lib/reading_list_ex_web/views/search_view.ex
@@ -1,3 +1,10 @@
 defmodule ReadingListExWeb.SearchView do
   use ReadingListExWeb, :view
+
+  def join_author_names(nil), do: ""
+  def join_author_names([]), do: ""
+  def join_author_names(authors_list) do
+    authors_list
+    |> Enum.join(", ")
+  end
 end
