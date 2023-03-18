47deb74791f7627910f5cc854d2208ed9c8b36cb
corsen-olbin <corbinolsen97@gmail.com>
2023-03-18 17:22:23 -0500

    almost have animation working on liveview

--- lib/reading_list_ex_web/live/book_feed_live.ex
+++ lib/reading_list_ex_web/live/book_feed_live.ex
@@ -2,20 +2,17 @@ defmodule ReadingListExWeb.BookFeedLive do
   use ReadingListExWeb, :live_view
 
   @topic "books:live"
+  @time_to_update 3
 
   def render(assigns) do
     ~H"""
     <h1>Books</h1>
-    <table>
 
-      <tbody>
+      <ul>
       <%= for book <- @books do %>
-        <tr>
-          <td><%= book.body %></td>
-        </tr>
+        <li class="list_item" mod-first={@mod_first == book.state.body} mod-last={@mod_last == book.state.body}><%= book.state.body %></li>
       <% end %>
-      </tbody>
-    </table>
+      </ul>
     """
   end
 
@@ -26,7 +23,30 @@ defmodule ReadingListExWeb.BookFeedLive do
   end
 
   def handle_info(%{topic: @topic, payload: state}, socket) do
-    remaining = Enum.take(socket.assigns.books, 4)
-    {:noreply, assign(socket, books: [state | remaining])}
+    now = DateTime.utc_now()
+    { add_transitions, new_books } = case socket.assigns.books do
+      [] -> { true, [%{state: state, time: now}] }
+      list -> add_new_book_if_time_passed(list, state)
+    end
+    socket = if add_transitions, do: add_transitions_tags(socket, new_books), else: socket
+    {:noreply, assign(socket, books: new_books)}
+  end
+
+  defp add_new_book_if_time_passed(list = [head | _], new_state) do
+    now = DateTime.utc_now()
+    cond do
+      DateTime.diff(now, head.time) > @time_to_update ->
+        remaining = Enum.take(list, 5)
+        { true, [%{ state: new_state, time: now } | remaining] }
+
+      true -> { false, list }
+    end
+  end
+
+  defp add_transitions_tags(socket, books) do
+    get_body_at_index = fn books, x -> Enum.at(books, x, %{}) |> Map.get(:state, %{}) |> Map.get(:body) end
+    socket
+    |> assign(:mod_first, get_body_at_index.(books, 0))
+    |> assign(:mod_last, get_body_at_index.(books, 5))
   end
 end
