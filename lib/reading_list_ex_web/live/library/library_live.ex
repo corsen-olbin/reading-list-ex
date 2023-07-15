7c8c9010ad0388ed93c4a36289a4db67723c0eeb
corsen-olbin <corbinolsen97@gmail.com>
2023-07-15 14:23:10 -0600

    start of moving library list view over to liveview

---
+++ lib/reading_list_ex_web/live/library/library_live.ex
@@ -0,0 +1,30 @@
+defmodule ReadingListExWeb.LibraryLive do
+  use ReadingListExWeb, :live_view
+
+  def mount(_params, _session, socket), do: {:ok, socket}
+
+  def handle_params(_params, _url, socket) do
+    {:noreply, assign_books(socket)}
+  end
+
+  alias ReadingListEx.Library
+
+  def assign_books(socket) do
+    profile = socket.assigns.current_profile
+
+    books = Library.get_profile_books_by_profile(profile)
+    assign(socket, :books, books)
+  end
+
+  def separate_author_names(nil), do: ""
+
+  def separate_author_names(authors_string) do
+    authors_string
+    |> String.split(";")
+    |> Enum.join(", ")
+  end
+
+  def change_to_https(nil), do: ""
+  def change_to_https("http://" <> rest_url), do: "https://#{rest_url}"
+  def change_to_https(url), do: url
+end
